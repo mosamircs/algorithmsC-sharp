5cfe70bcf86b54ed1d2b75b4e74db5a4519baea5
mosamircs <mosamircs@gmail.com>
2021-07-29 16:49:39 +0200

    can now be dynamic in input from console

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+
 namespace algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int [] list = {1,3,5,7,9,11,13,15,17};
+            
+            Console.WriteLine("Enter the list of elements separated by spaces: ");
+            int [] list = Array.ConvertAll(Console.ReadLine().Split(' '),temp =>Convert.ToInt32(temp ));
+            Console.WriteLine("Enter the element to search for: ");
+            int element =Convert.ToInt32(Console.ReadLine());
             Search s1 = new Search();
-            int? index = s1.BinarySearch(list,17);
+            int? index = s1.BinarySearch(list,element);
             Console.WriteLine(index);
         }
     }
