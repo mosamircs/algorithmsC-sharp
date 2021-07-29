dce769c39be19ba40ee345e8b375819c3828a36f
mosamircs <mosamircs@gmail.com>
2021-07-29 16:17:35 +0200

    binary search completed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
 namespace algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int [] list = {1,3,5,7,9,11,13,15,17};
+            Search s1 = new Search();
+            int? index = s1.BinarySearch(list,17);
+            Console.WriteLine(index);
         }
     }
 }
---
+++ Search.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace algorithms
+{
+    class Search
+    {
+        /**
+        The binary_search function takes a sorted array and an item. If the
+        item is in the array, the function returns its position. You’ll keep track
+        of what part of the array you have to search through. At the beginning,
+        this is the entire array:
+        */
+        public int? BinarySearch(int [] list , int element)
+        {
+            int low = 0;//first element in list
+            int high = list.Length - 1;//last element in list
+            int mid;
+            int guess;
+            while(low <= high)
+            {
+                mid = (low + high ) / 2;//gets the middle element
+                guess = list[mid];
+                if(element == guess)//found the item
+                {
+                    return mid;
+                }
+                else if(element < guess)//case element < middle go and search in left part of the list
+                {
+                    high = mid - 1;
+                }
+                else 
+                {
+                    low = mid +1;
+                }
+                
+            }
+            Console.WriteLine("Not Found");
+            return null;
+        }
+    }
+}
