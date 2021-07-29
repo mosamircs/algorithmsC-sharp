3c87b72a9b2dd65a0f990e81a0d484037a61b13f
mosamircs <mosamircs@gmail.com>
2021-07-29 18:23:46 +0200

    create a data file and get input from it

--- Program.cs
+++ Program.cs
@@ -1,28 +1,29 @@
 ﻿using System;
-
+using StreamWriter;
 namespace algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //reads the list
-            Console.WriteLine("Enter the list of elements separated by spaces: ");
-            int [] list = Array.ConvertAll(Console.ReadLine().Split(' '),temp =>Convert.ToInt32(temp ));
+            CreateData();
+            // //reads the list
+            // Console.WriteLine("Enter the list of elements separated by spaces: ");
+            // int [] list = Array.ConvertAll(Console.ReadLine().Split(' '),temp =>Convert.ToInt32(temp ));
 
-            //reads the element to search in the list
-            Console.WriteLine("Enter the element to search for: ");
-            int element =Convert.ToInt32(Console.ReadLine());
+            // //reads the element to search in the list
+            // Console.WriteLine("Enter the element to search for: ");
+            // int element =Convert.ToInt32(Console.ReadLine());
             
-            //reads search algorithm choice
-            Console.WriteLine("Enter Search Algorithm (linear or binary): " );
-            string seachChoice = Console.ReadLine();
+            // //reads search algorithm choice
+            // Console.WriteLine("Enter Search Algorithm (linear or binary): " );
+            // string seachChoice = Console.ReadLine();
 
-            var watch = new System.Diagnostics.Stopwatch();
-            watch.Start();
-            SwitchSearch(seachChoice, list,element);
-            watch.Stop();
-            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");    
+            // var watch = new System.Diagnostics.Stopwatch();
+            // watch.Start();
+            // SwitchSearch(seachChoice, list,element);
+            // watch.Stop();
+            // Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");    
 
         }
         public static void SwitchSearch(string searchChoice,int [] list,int element)
@@ -44,5 +45,19 @@ namespace algorithms
                 }
             }
         }
+        public static void CreateData()
+        { 
+     //Random r = new Random(); 
+     int number = 0; 
+     using (StreamWriter writer = File.AppendText(@"d:\\algorithms\testfilebinary.txt"))
+     {
+         for (int i = 1; i < 1000; i++)
+         {
+             writer.Write(i + " ");
+         }
+        writer.Write(959);
+        writer.Write("binary");
+     }
+}
     }
 }
